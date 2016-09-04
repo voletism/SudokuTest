729ad897ab42b50046915c7ed1f3360a44920acd
voletism <smsvoleti@gmail.com>
2016-09-04 11:25:50 +0300

    fixed  column checking and removed random number

--- SudokuGame/src/com/home/voleti/SudokuBoard.java
+++ SudokuGame/src/com/home/voleti/SudokuBoard.java
@@ -56,29 +56,42 @@ public class SudokuBoard {
 	     **  puzzle will be generated, to play @@
 	     */
 		public void generateRandomSudokuBoard() {
-	    	 int indexRow = 0;
-	    	 int indexColl = 0;
+	    	 int randomRow = 0;
+	    	 int randomColl = 0;
 	    	 int index = 0;
 	    	 
 	    	 // Let's generate some random numbers, and place them on the board
 	    	 // if the size of the board is 4, then let's generate 4 numbers to place them on the board
 	    	 
     		 for (int i = 0; i < this.size; i++) {
-	    		 indexRow = (int)(Math.random() * this.size);
-	    		 indexColl = (int)(Math.random() * this.size);
-	    		 index = indexRow*10 + indexColl;
+    			 Set <Integer> colSet = new HashSet<Integer>(this.size);
+    			 
+    			 //randomRow = (int)(Math.random() * this.size);
+	    		 // randomColl = (int)(Math.random() * this.size);
+    			 randomRow = i;
+	    		 randomColl = (this.size -i ) -1 ;
+	    		 
+	    		 index = randomRow*10 + randomColl;
 	    		 busyIndices.add(index);
-	    		 SingleCell littleCell = new SingleCell(indexRow+1, indexRow, indexColl);
-	    		 totalCells[indexRow][indexColl] = littleCell;
-	    	    }
-	    	
-	    	 Iterator<Integer>  myIt = busyIndices.iterator();
-	    	    while( myIt.hasNext()) {
-	    	    	myIt.next();
-	    	    }
-	    	    System.out.println("generate random SudokuBoard");
-	    	 	displayTheSudokuBoard(totalCells);    	 
-	    	 
+	    		 
+	    		// if the value  at the random row and Collumm is -1, then use the random number,
+	    		// let  us findout existing numbers in the collumn and add them to Collumn set
+	    		 // to add at the cell, also see the number is valid to add at location
+	    		 
+	    		 int value = totalCells[randomRow][randomColl].getValue();    		 
+	    		 
+	    		 if ( value == -1 )  {
+	    			 for (int j = 0; j < this.size; j++) {
+	    				 int temp = totalCells[j][randomColl].getValue();
+	    				 if ( !colSet.contains(temp))
+	    					 	colSet.add(temp);
+	    		 		}
+	    		  setTheValidNumber(randomRow,randomColl, colSet);
+	    		 }
+    		 }  // end of for loop
+	    		
+	    	 System.out.println("generate random SudokuBoard");
+	    	 displayTheSudokuBoard(totalCells);    	     	 
 	     }
 	     
 		/*
@@ -90,36 +103,57 @@ public class SudokuBoard {
 	    	 // lets check the value of each cell and fill them accordingly
 	    	 for (int i = 0; i < this.size; i++){
 	    		 Set <Integer> rowSet = new HashSet<Integer>(this.size);
-	    		 	/* for (int j = 0; j < this.size; j++) {
+	    		 	 for (int j = 0; j < this.size; j++) {
 	    			 	int value = totalCells[i][j].getValue();
 	    			 	if ( ! rowSet.contains(value) ) {
 				    		rowSet.add( value);
 	    			 	}
 	    		 	} // end of for loop j
-	    		 	*/
-	    			
+	    		 		    			
 	    		  // Now, lets check the values in the row set and add the missing numbers	    		 
 	    		 	for (int k = 0; k < this.size; k++) {
 	    			 	int getValue = totalCells[i][k].getValue();
-	    			 	int count = 1;
-	    			 	// If the value is  -1, then it empty, let's fill with valid value
-	    			 	if ( getValue == -1 )  {
-	    			 		for (int l = 0; l < this.size; l++) {   			 			 
-	    			 				if ( ! rowSet.contains(count) ) {
-	    			 					rowSet.add( count);
-	    			 					totalCells[i][k].setValue(count);
-	    			 					totalCells[i][k].setRowIndex(i);
-	    			 					totalCells[i][k].setRowIndex(k);
-	    			 					break;
-	    			 				}
-	    			 				count++;
-	    			 			} // end of for loop which sets new count
-	    			 		}  // if value is invalid -1;
-	    		 		} // end of for loop k
+	    			 	
+	    			 	 // let us search in the collum set, to see if the value is not exists
+	    			 	 // creating a new colSet here...
+	    			 	 Set <Integer> colSet = new HashSet<Integer>(this.size);
+	    			 	 
+	    			 	// If the value is  -1, then its empty, let's fill with valid value
+	    			 	 if ( getValue == -1 )  {
+	    	    			 for (int j = 0; j < this.size; j++) {
+	    	    				 int temp = totalCells[j][k].getValue();
+	    	    				 if ( !colSet.contains(temp))
+	    	    					 	colSet.add(temp);
+	    	    		 		} // end of for loop j
+	    	    		  setTheValidNumber(i,k, rowSet);
+	    	    		 }
+	    		 	} // end of for loop k
 	    	 	} // end of for loop i
 	    	 
 	    	 System.out.println("Solving the puzzle SudokuBoard");
 	    	 displayTheSudokuBoard(totalCells);
+	     }
+	     
+	     /*
+	     @@ Get the Valid number for the cell  to be added to the Board @@
+	     **
+	     */
+	     private void setTheValidNumber(int row, int coll, Set<Integer> rowSet) {
+	    	 int count = 1;
+	    	 int index = 1;
+	    	 
+	    	 for (int l = 0; l < this.size; l++) {   			 			 
+				if ( ! rowSet.contains(count) ) {
+					rowSet.add( count);
+					SingleCell littleCell = new SingleCell(count, row, coll);
+		    		totalCells[row][coll] = littleCell;
+					index = row*10 + coll;
+		    		busyIndices.add(index);
+		    		// System.out.println("busyIndices =" +  busyIndices.toString());
+		    		return;
+				}
+				count++;
+			} // end of for loop which sets new count
 	     }
 	      
 	     /*
